b5de21690b6c032888bb92692a23d3d1b16b93a1
klimisnotavailable <klimgrigor28@gmail.com>
2024-05-08 17:07:21 +0300

    слухач подій на img

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,7 +3,7 @@ export default function ImageCard({ image, setIsOpen, setImage }) {
     const { raw, full, regular, small, thumb } = image.urls;
     const { id } = image.id;
 
-    return <div id={id} onClick={() => { setIsOpen(true); setImage(regular)}}>
-        <img src={small} alt="" width={50} height={50}/>
+    return <div id={id} >
+        <img onClick={() => { setIsOpen(true); setImage(regular)}} src={small} alt="" width={50} height={50}/>
     </div>
 }
